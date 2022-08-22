f044e50e14efc14b767b7d3708d0dec6b07591c7
bakemonostan <ehizomokhomion@gmail.com>
2022-08-22 16:45:35 -0700

    first commit, functionalities working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import jobs from './data.json';
+import { useEffect, useState } from 'react';
+import Joblist from './Joblist';
 
 function App() {
+  const [jobLists, setjobLists] = useState([]);
+  const [filteredCat, setFilteredCat] = useState([]);
+  const [cat, setCat] = useState([]);
+
+  const handleClick = (val) => {
+    const newList = jobLists.filter((jobs) => {
+      const { tools, languages, role } = jobs;
+      if (role === val) {
+        return role === val;
+      }
+      if (tools.includes(val)) {
+        return tools !== val;
+      }
+      if (languages.includes(val)) {
+        return languages !== val;
+      }
+      return jobs.level === val;
+    });
+    console.log(newList);
+    setFilteredCat(newList);
+    if (!cat.includes(val)) {
+      setCat([...cat, val]);
+    }
+  };
+
+  const clearCat = (arg) => {
+    if (cat.includes(arg)) {
+      setFilteredCat(jobs);
+      setCat([]);
+    }
+  };
+
+  useEffect(() => {
+    setjobLists(jobs);
+    setFilteredCat(jobs);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='header'>
+        <div className='header_img'></div>
+
+        {cat.map((item) => {
+          return (
+            <span style={{ marginLeft: '1rem', backgroundColor: 'gray' }}>
+              {item} <button onClick={() => clearCat(item)}>X</button>
+            </span>
+          );
+        })}
       </header>
+      <main>
+        {filteredCat.map((item) => {
+          return (
+            <Joblist
+              key={item.id}
+              {...item}
+              handleClick={handleClick}
+              // filterJobs={filterJobs}
+            />
+          );
+        })}
+      </main>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Joblist.jsx
@@ -0,0 +1,86 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const Joblist = ({
+  company,
+  new: isNew,
+  featured,
+  position,
+  postedAt,
+  contract,
+  location,
+  logo,
+  role,
+  tools,
+  level,
+  languages,
+  id,
+  handleClick,
+}) => {
+  useEffect(() => {}, []);
+
+  return (
+    <section className='main'>
+      <div className={`card ${id === 1 || id === 2 ? 'border-right' : ' '}`}>
+        <div className='info'>
+          <div className='logo-contanier'>
+            <img src={logo} alt={company} className='logo' />
+          </div>
+          <div className='company_info'>
+            <h4 className='company'>
+              {company} {isNew && <span className='new'>New!</span>}
+              {featured && <span className='new'>Featured!</span>}
+            </h4>
+
+            <p className='position'>{position}</p>
+            <div className='others'>
+              <ul className='list'>
+                <li>{postedAt}</li>
+                <li>{contract}</li>
+                <li>{location}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className='roles'>
+          <button type='button' value={role} onClick={(e) => handleClick(role)}>
+            {role}
+          </button>
+
+          <button
+            type='button'
+            value={level}
+            onClick={(e) => handleClick(level)}
+          >
+            {level}
+          </button>
+          {languages.map((item, index) => {
+            return (
+              <button
+                key={index}
+                type='button'
+                value={item}
+                onClick={(e) => handleClick(item)}
+              >
+                {item}
+              </button>
+            );
+          })}
+          {tools.map((item, index) => {
+            return (
+              <button
+                key={index}
+                type='button'
+                value={item}
+                onClick={(e) => handleClick(item)}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Joblist;
